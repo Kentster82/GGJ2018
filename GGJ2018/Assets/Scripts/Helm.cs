f99f0ba43823e947a49cf421703e2bcb92a9469b
Unknown <kdeppmeier@yahoo.com>
2018-01-27 10:12:08 -0800

    Edited Helm script

--- GGJ2018/Assets/Scripts/Helm.cs
+++ GGJ2018/Assets/Scripts/Helm.cs
@@ -18,4 +18,16 @@ public class Helm:MonoBehaviour
         //Get the new setting and report it to the ship class
         Debug.Log("Throttle value:" + slider.value);
     }
+
+    public void ReleaseWheel(Image image)
+    {
+
+    }
+
+    public void TurnWheel(Image image)
+    {
+        float xAxis = Input.mousePosition.x - image.transform.position.x;
+        float yAxis = Input.mousePosition.y - image.transform.position.y;
+        Debug.Log(xAxis + " " + yAxis);
+    }
 }
