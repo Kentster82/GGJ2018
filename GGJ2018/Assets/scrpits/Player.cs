bf21fb9ab1abc42a859dbc3991edbf19bdea3b7d
Kent Pluntze <Kpluntze@uci.edu>
2018-01-26 22:47:45 -0800

    if this breaks something im branching

---
+++ GGJ2018/Assets/scrpits/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+    bool usable;
+	// Use this for initialization
+	void Start () {
+        usable = true;
+	}/*end of start*/
+	
+	// Update is called once per frame
+	void Update () {
+		if (usable == true) /*make sure we want to move the player*/
+        {
+
+        }
+	}/*end of update*/
+
+    void FixedUpdate()
+    {
+
+    }/*end of Fixedupdate*/
+}
