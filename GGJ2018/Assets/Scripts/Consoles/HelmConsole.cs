ae48cd0b6e0402bff53846d281fa0354c013137b
jwardlaw <jwardlaw@uci.edu>
2018-01-27 14:49:39 -0800

    Consoles are basically working

---
+++ GGJ2018/Assets/Scripts/Consoles/HelmConsole.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelmConsole : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject UIPanel;
+    public Camera helmCamera;
+
+    public float interactDistance = 2f;
+
+    // Use this for initialization
+    void Start()
+    {
+        helmCamera = GameObject.Find("helmCamera").GetComponent<Camera>();
+        UIPanel = GameObject.Find("HelmPanel");
+        player = GameObject.Find("Player");
+        UIPanel.SetActive(false);
+    }
+
+    public void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(1) && Vector2.Distance(this.transform.position, player.transform.position) < interactDistance)
+        {
+            UIPanel.SetActive(true);
+            Camera.main.enabled = false;
+            helmCamera.enabled = true;
+        }
+    }
+
+    public void ExitConsole()
+    {
+        UIPanel.SetActive(false);
+        helmCamera.enabled = false;
+        Camera.main.enabled = true;
+    }
+}
---
+++ GGJ2018/Assets/Scripts/Consoles/NavigationConsole.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavigationConsole : MonoBehaviour {
+
+    public GameObject player;
+    public GameObject UIPanel;
+    public Camera navCamera;
+
+    public float interactDistance = 2f;
+    // Use this for initialization
+    void Start ()
+    {
+        navCamera = GameObject.Find("navCamera").GetComponent<Camera>();
+        UIPanel = GameObject.Find("NavigationPanel");
+        player = GameObject.Find("Player");
+        UIPanel.SetActive(false);
+    }
+
+    public void OnMouseOver()
+    {
+        if(Input.GetMouseButtonDown(1) && Vector2.Distance(this.transform.position, player.transform.position) < interactDistance)
+        {
+            UIPanel.SetActive(true);
+            Camera.main.enabled = false;
+            navCamera.enabled = true;
+        }
+    }
+
+    public void ExitConsole()
+    {
+        UIPanel.SetActive(false);
+        navCamera.enabled = false;
+        Camera.main.enabled = true;
+    }
+}
