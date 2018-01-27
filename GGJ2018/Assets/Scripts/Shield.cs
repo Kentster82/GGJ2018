6ca2de8022ee4ec7f98bc9266fd962c34bc2a1df
Unknown <kdeppmeier@yahoo.com>
2018-01-27 15:45:45 -0800

    Added Shield Console

---
+++ GGJ2018/Assets/Scripts/Shield.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield : MonoBehaviour
+{
+    private GameObject ship;
+
+    void Awake()
+    {
+        ship = GameObject.Find("Ship");
+        GameObject shield = GameObject.Find("ShieldPanel");
+        shield.SetActive(false);
+    }
+
+    public void SetShieldOn(bool onOff)
+    {
+        //Shield in ShipController is set on/off
+        ship.GetComponent<ShipController>().shieldActive = onOff;
+
+        //Debug.Log("Shield on: " + onOff);
+    }
+}
