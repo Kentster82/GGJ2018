eb35de2a69090a70ac4b35fff5bff4d9264747d2
jwardlaw <jwardlaw@uci.edu>
2018-01-27 10:08:34 -0800

    fixed the gitignore and added some basic shit

---
+++ GGJ2018/Assets/Scripts/ShipController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour {
+
+    // scalar from -1 to 1 for use with helm
+    public float throttle = 0;
+    // if health reaches zero, you die
+    public float health;
+    // take damage before health, regenerate over time
+    public float shields;
+
+    //How fast the ship accelerates
+    public float thrust = 5f;
+    //public reference to ship's transform
+    public Transform shipTransform;
+
+    private Rigidbody2D rb;
+    
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        shipTransform = this.transform;
+	}
+
+    void FixedUpdate()
+    {
+        rb.AddForce(transform.up * thrust * throttle);
+    }
+
+    void Update ()
+    {
+		
+	}
+}
