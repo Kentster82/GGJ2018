6016cfd171bbd912f7e18b158f4c53dd7261e8ae
Kent Pluntze <kent.pluntze@gmail.com>
2018-01-27 11:40:42 -0800

    Player control v1

---
+++ GGJ2018/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+    // Use this for initialization
+    public float speed;
+    Vector3 mousePos;
+    Camera c;
+	void Start () {
+        mousePos = new Vector3();
+        c = Camera.main;
+        speed = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+          mousePos = c.ScreenToWorldPoint(Input.mousePosition);
+          transform.LookAt(mousePos);
+          transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y, transform.localEulerAngles.z);
+
+          if (Input.GetMouseButton(0))
+          {
+              transform.position += (-transform.position + mousePos).normalized * speed * Time.deltaTime;
+              transform.position = new Vector3(transform.position.x, 0, transform.position.z);
+          }
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+}
