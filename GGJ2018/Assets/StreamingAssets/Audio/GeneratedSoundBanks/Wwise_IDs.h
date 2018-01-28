4d095bccc15c2d1d0e639589b135bc547a70d05e
VinCMG <vincentchang9999@gmail.com>
2018-01-27 18:58:27 -0800

    Split sound banks into separate ones for each console

---
+++ GGJ2018/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,123 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID AIRLOCKMUSICSTART = 3633860693U;
+        static const AkUniqueID BREAKAIRLOCK = 3240935707U;
+        static const AkUniqueID BREAKENGINES = 1745692283U;
+        static const AkUniqueID BREAKHELM = 3662416074U;
+        static const AkUniqueID BREAKNAVIGATION = 1682081864U;
+        static const AkUniqueID BREAKSHIELDS = 3957382364U;
+        static const AkUniqueID CONSOLEENTER = 3378699830U;
+        static const AkUniqueID CONSOLEEXIT = 3324615484U;
+        static const AkUniqueID ENGINESMUSICSTART = 682262605U;
+        static const AkUniqueID FIXAIRLOCK = 3491753779U;
+        static const AkUniqueID FIXENGINES = 3611661827U;
+        static const AkUniqueID FIXHELM = 3614165602U;
+        static const AkUniqueID FIXNAVIGATION = 537538816U;
+        static const AkUniqueID FIXSHIELDS = 3233625636U;
+        static const AkUniqueID HELMMUSICSTART = 1651135938U;
+        static const AkUniqueID NAVIGATIONMUSICSTART = 3744199996U;
+        static const AkUniqueID PCSTARTMOVING = 178998712U;
+        static const AkUniqueID PCSTOPMOVING = 3432613688U;
+        static const AkUniqueID SHIELDSMUSICSTART = 707167398U;
+        static const AkUniqueID SHIPHIT = 2174516238U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace AIRLOCKSTATE
+        {
+            static const AkUniqueID GROUP = 3693547105U;
+
+            namespace STATE
+            {
+                static const AkUniqueID BROKEN = 231230354U;
+                static const AkUniqueID WORKING = 1992928594U;
+            } // namespace STATE
+        } // namespace AIRLOCKSTATE
+
+        namespace ENGINESSTATE
+        {
+            static const AkUniqueID GROUP = 3180446217U;
+
+            namespace STATE
+            {
+                static const AkUniqueID BROKEN = 231230354U;
+                static const AkUniqueID WORKING = 1992928594U;
+            } // namespace STATE
+        } // namespace ENGINESSTATE
+
+        namespace HELMSTATE
+        {
+            static const AkUniqueID GROUP = 908130836U;
+
+            namespace STATE
+            {
+                static const AkUniqueID BROKEN = 231230354U;
+                static const AkUniqueID WORKING = 1992928594U;
+            } // namespace STATE
+        } // namespace HELMSTATE
+
+        namespace NAVIGATIONSTATE
+        {
+            static const AkUniqueID GROUP = 3426717734U;
+
+            namespace STATE
+            {
+                static const AkUniqueID BROKEN = 231230354U;
+                static const AkUniqueID WORKING = 1992928594U;
+            } // namespace STATE
+        } // namespace NAVIGATIONSTATE
+
+        namespace SHIELDSSTATE
+        {
+            static const AkUniqueID GROUP = 2369853672U;
+
+            namespace STATE
+            {
+                static const AkUniqueID BROKEN = 231230354U;
+                static const AkUniqueID WORKING = 1992928594U;
+            } // namespace STATE
+        } // namespace SHIELDSSTATE
+
+    } // namespace STATES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID AIRLOCK = 3901468890U;
+        static const AkUniqueID BGM1 = 1189781958U;
+        static const AkUniqueID ENGINES = 759487314U;
+        static const AkUniqueID HELM = 3632828377U;
+        static const AkUniqueID NAVIGATION = 1082482811U;
+        static const AkUniqueID SHIELDS = 289290189U;
+        static const AkUniqueID SOUNDEFFECTS = 3898083304U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID COMMUNICATION = 530303819U;
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
