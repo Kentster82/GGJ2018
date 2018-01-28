3e9b942a6e20347f0e519eff6de1bc062225e876
Unknown <kdeppmeier@yahoo.com>
2018-01-27 17:09:08 -0800

    Removed duplicate files, fixed shipcontroller

--- GGJ2018/Assets/Scripts/ShipController.cs
+++ GGJ2018/Assets/Scripts/ShipController.cs
@@ -108,7 +108,7 @@ public class ShipController : MonoBehaviour {
                 break;
             default:
                 break;
-}
+        }
     }
 
     IEnumerator eBrake()
@@ -130,12 +130,3 @@ public class ShipController : MonoBehaviour {
         rb.drag = 0f;
     }
 }
-
-        switch (randomNum)
-        {
-            case 0:
-                shieldStationActive = false;
-                break;
-            default:
-                break;
-        }
